1f89c4f1f4bbbb03a541d3ce014ea48a1d356505
dyy <yayi_du@kingdee.com>
2020-09-15 13:44:23 +0800

    Re-add newlines in tests after splitting.
    the parser doesn't do a good job at dealing with eof in this version.

--- tests/lib.rs
+++ tests/lib.rs
@@ -3,14 +3,14 @@ extern crate nom_sql_learning;
 use std::fs::File;
 use std::io::Read;
 
-fn parse_queryset(queries: Vec<&str>) {
+fn parse_queryset(queries: Vec<String>) {
     let mut parsed_ok = Vec::new();
     let mut parsed_err = 0;
 
     for query in queries.iter() {
-        match nom_sql_learning::parser::parse_query(query) {
-            Ok(q) => {
-                parsed_ok.push(q);
+        match nom_sql_learning::parser::parse_query(&query) {
+            Ok(_) => {
+                parsed_ok.push(query);
             }
             Err(_) => parsed_err += 1,
         }
@@ -31,7 +31,14 @@ fn hotcrp_queries() {
     f.read_to_string(&mut s).unwrap();
 
     let lines: Vec<&str> = s.split("\n").collect();
-    let lines: Vec<&str> = s.lines().filter(|l| !l.starts_with("#")).collect();
+    let lines: Vec<String> = s.lines().filter(|l| !l.starts_with("#"))
+        .map(|l| {
+            if !(l.ends_with("\n") || l.ends_with(";")) {
+                String::from(l) + "\n"
+            } else {
+                String::from(l)
+            }
+        }).collect();
 
     println!("Loaded {} total queries", lines.len());
 
@@ -47,7 +54,15 @@ fn hyrise_test_queries() {
     file.read_to_string(&mut s).unwrap();
 
     let lines: Vec<&str> = s.split("\n").collect();
-    let lines: Vec<&str> = s.lines().filter(|l| !l.starts_with("#")).collect();
+    let lines: Vec<String> = s.lines().filter(|l| !l.starts_with("#"))
+        .map(|l| {
+            if !(l.ends_with("\n") || l.ends_with(";")) {
+                String::from(l) + "\n"
+            } else {
+                String::from(l)
+            }
+        }).collect();
+
     println!("Loaded {} Hyrise queries", lines.len());
 
     parse_queryset(lines);
